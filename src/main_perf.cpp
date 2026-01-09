6a45b99b95205fe5b2cbc7cc521f31a6ddf59c8e
Arinc Demir <arinc.demir2002@gmail.com>
2026-01-09 13:16:03 +0300

    created a main file to run with linux perf, gaining insight on the runtime

--- src/main_perf.cpp
+++ src/main_perf.cpp
@@ -18,6 +18,7 @@ int main(int argc, char **argv)
     const auto &allInputs = binary_row_reader::readInputFile("/home/arinc/workspace/do-verify/data/fullsuite/AbsentAQ/Discrete/1M/AbsentAQ10.row.bin");
     for (int i = 0; i < 100; i++)
     {
+        std::vector<bool> propositionInputs(2);
         IntervalSetHolder holder = newHolder(1000);
         DiscreteNode q{empty(holder), false, NodeType::PROPOSITION, 0, 0, 0, 0};
         DiscreteNode p{empty(holder), false, NodeType::PROPOSITION, 0, 0, 0, 0};
@@ -28,9 +29,12 @@ int main(int argc, char **argv)
         DiscreteNode always{empty(holder), false, NodeType::ALWAYS, 0, 5, 0, B_INFINITY};
         std::vector<DiscreteNode> nodes{q, p, once, notNode, since, implies, always};
 
+        //TODO removed vector allocations for propositions.
         for (size_t i = 0; i < allInputs.size(); i++)
         {
-            run_evaluation(nodes, holder, allInputs[i].time, {allInputs[i].q, allInputs[i].p});
+            propositionInputs[0] = allInputs[i].q;
+            propositionInputs[1] = allInputs[i].p;
+            run_evaluation(nodes, holder, allInputs[i].time, propositionInputs);
             swapBuffers(holder);
         }
         destroyHolder(holder);
